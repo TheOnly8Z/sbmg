4552f5e776e53734c30c91b808b23c594bdd3c6b
TheOnly8Z <8888888zombies@gmail.com>
2020-12-31 16:18:38 +0800

    Flag Slowdown

--- lua/sbmg/minigames/ctf.lua
+++ lua/sbmg/minigames/ctf.lua
@@ -23,6 +23,7 @@ MINIGAME.Options = {
     ["flag_return_time"] = {type = "i", min = 1, default = 60},
     ["flag_cap_need"] = {type = "b", default = false},
     ["flag_hold"] = {type = "b", default = true},
+    ["flag_slowdown"] = {type = "i", min = 0, max = 1, default = 0.75},
 }
 
 function MINIGAME:GetParticipants()
